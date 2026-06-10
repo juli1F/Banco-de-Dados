83897e8150f4bb9f02ddee60d115202407653f57
Juliana Barbosa <juliana.barbosa2054@gmail.com>
2026-06-10 17:47:26 -0300

    Add files via upload

--- produtos (1).sql
+++ produtos (1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07/06/2026 às 20:48
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Tempo de geração: 10-Jun-2026 às 22:47
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,23 +20,21 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `produtos`
 --
-CREATE DATABASE IF NOT EXISTS `produtos` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `produtos`;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `categoria`
+-- Estrutura da tabela `categoria`
 --
 
 CREATE TABLE `categoria` (
   `idcategoria` int(11) NOT NULL,
   `categoria` varchar(100) NOT NULL,
   `descricao` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `categoria`
+-- Extraindo dados da tabela `categoria`
 --
 
 INSERT INTO `categoria` (`idcategoria`, `categoria`, `descricao`) VALUES
@@ -63,7 +61,7 @@ INSERT INTO `categoria` (`idcategoria`, `categoria`, `descricao`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `produtos`
+-- Estrutura da tabela `produtos`
 --
 
 CREATE TABLE `produtos` (
@@ -75,48 +73,47 @@ CREATE TABLE `produtos` (
   `datacadastro` date NOT NULL,
   `ativo` bit(1) NOT NULL,
   `marca` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `produtos`
+-- Extraindo dados da tabela `produtos`
 --
 
 INSERT INTO `produtos` (`idproduto`, `idcategoria`, `nomeproduto`, `preco`, `qtde`, `datacadastro`, `ativo`, `marca`) VALUES
-(1, 3, 'Cortina de 3m', 495.65, 20, '2026-05-27', b'1', 'Bella Janela'),
-(2, 2, 'Detergente Ypê', 2.99, 100, '2026-04-10', b'1', 'Ypê'),
-(3, 7, 'Violão', 899.99, 15, '2026-05-30', b'1', 'Tagima'),
-(4, 8, 'Cabo da fonte', 20.00, 50, '2024-09-12', b'1', 'Gigabyte'),
-(5, 9, 'Camiseta Social', 70.00, 47, '2023-01-09', b'1', 'Renner'),
-(6, 10, 'Salto alto', 190.00, 39, '2025-06-01', b'1', 'YSL'),
-(7, 11, 'Livro de Romance', 50.00, 128, '2021-08-01', b'1', 'Panini'),
-(8, 12, 'Chocolate', 10.00, 230, '2020-07-22', b'1', 'Cacau Show'),
-(9, 13, 'Suco de Goiaba', 12.00, 170, '2023-09-03', b'1', 'Natural One'),
-(10, 14, 'Painel da TV', 350.00, 10, '2019-04-23', b'1', 'DJ Móveis'),
-(11, 15, 'Bola de futebol', 90.00, 49, '2018-03-28', b'1', 'Adidas'),
-(12, 16, 'Blush', 15.00, 130, '2017-02-17', b'1', 'Dior'),
-(13, 17, 'Café', 35.00, 40, '2017-09-07', b'1', 'Pilão'),
-(14, 18, 'Cimento 10kg', 150.00, 10, '2015-02-07', b'1', 'Votoran'),
-(15, 19, 'Boneca', 78.00, 207, '2016-01-02', b'1', 'Barbie');
+(1, 3, 'Cortina de 3m', '495.65', 20, '2026-05-27', b'1', 'Bella Janela'),
+(3, 7, 'Violão', '899.99', 15, '2026-05-30', b'1', 'Tagima'),
+(4, 8, 'Cabo da fonte', '20.00', 50, '2024-09-12', b'1', 'Gigabyte'),
+(5, 9, 'Camiseta Social', '70.00', 47, '2023-01-09', b'1', 'Renner'),
+(6, 10, 'Salto alto', '190.00', 39, '2025-06-01', b'1', 'YSL'),
+(7, 11, 'Livro de Romance', '50.00', 128, '2021-08-01', b'1', 'Panini'),
+(8, 12, 'Chocolate', '10.00', 230, '2020-07-22', b'1', 'Cacau Show'),
+(9, 13, 'Suco de Goiaba', '12.00', 170, '2023-09-03', b'1', 'Natural One'),
+(10, 14, 'Painel da TV', '350.00', 10, '2019-04-23', b'1', 'DJ Móveis'),
+(11, 15, 'Bola de futebol', '90.00', 49, '2018-03-28', b'1', 'Adidas'),
+(12, 16, 'Blush', '15.00', 130, '2017-02-17', b'1', 'Dior'),
+(13, 17, 'Café', '35.00', 40, '2017-09-07', b'1', 'Pilão'),
+(14, 18, 'Cimento 10kg', '150.00', 10, '2015-02-07', b'1', 'Votoran'),
+(15, 19, 'Boneca', '78.00', 207, '2016-01-02', b'1', 'Barbie');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `categoria`
+-- Índices para tabela `categoria`
 --
 ALTER TABLE `categoria`
   ADD PRIMARY KEY (`idcategoria`);
 
 --
--- Índices de tabela `produtos`
+-- Índices para tabela `produtos`
 --
 ALTER TABLE `produtos`
   ADD PRIMARY KEY (`idproduto`),
   ADD KEY `idcategoria` (`idcategoria`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
@@ -132,11 +129,11 @@ ALTER TABLE `produtos`
   MODIFY `idproduto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- Restrições para tabelas despejadas
+-- Restrições para despejos de tabelas
 --
 
 --
--- Restrições para tabelas `produtos`
+-- Limitadores para a tabela `produtos`
 --
 ALTER TABLE `produtos`
   ADD CONSTRAINT `produtos_ibfk_1` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`idcategoria`);
